64b5147fd5b9884cc7c1eaad9dc864560092e575
Karlis Cars <karlis.cars@gmail.com>
2019-10-26 16:00:21 +0300

    adding Ui func, and some constrains

--- DayFinder/ViewController.swift
+++ DayFinder/ViewController.swift
@@ -10,10 +10,37 @@ import UIKit
 var str = "izmainja"
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dayText: UITextField!
+    @IBOutlet weak var monthText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    @IBOutlet weak var dayOfTheWeek: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         statusBarStyle()
+        
     }
+    
+    @IBAction func findMyWeek(_ sender: Any) {
+        let calendar = Calendar.current
+         var dateCompenets = DateComponents()
+        
+        guard let day = dayTextField.text, let month = monthTextField.text, let year = yearTextField.text else {return}
+        
+      
+      //  dateCompenets.day = Int(dayText.text!)
+       // dateCompenets.month = Int(monthText.text!)
+       // dateCompenets.year = Int(yearText.text!)
+        
+        let date = calendar.date(from: dateCompenets)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier:"lv_LV")
+        dateFormatter.dateFormat = "EEEE"
+        
+        let weekday = dateFormatter.string(from: date!)
+        let capitalizedWeekday = weekday.capitalizedresultLabel.text = capitalizedWeekday
+    }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
@@ -25,7 +52,11 @@ class ViewController: UIViewController {
         app.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
         
         self.navigationController?.navigationBar.scrollEdgeAppearance = app
+        
+        
     }
-
+   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    self.view.endEditing(true)
+    }
 }
 
